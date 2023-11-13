334f791d4e3e411402b28fc5af1b459b081e23f6
Fatih <fatihunal@outlook.com>
2023-11-13 12:15:28 +0300

    start

---
+++ android/app/src/main/kotlin/com/fthdev/architecture/architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fthdev.architecture.architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/feature/home/view/widget/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:architecture/feature/home/view/mixin/home_view_mixin.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> with HomeViewMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
